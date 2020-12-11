77bcb247606e57f39b8bb64b496b46e9582f0fd0
saddamhr <saddambubt65@gmail.com>
2020-12-11 10:58:29 +0600

    view encapsulation

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+// import { EventEmitter } from 'protractor';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+    this.serverCreated.emit({
+       serverName: this.newServerName,
+        serverContent: this.newServerContent 
+      })
+  }
+
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
+
+}
